dcabb74c5003b063f56150ca68d6176e39052409
Allemon <allemon3@gmail.com>
2023-02-02 12:55:18 +0100

    username now has to be 5 to 20 characters long

--- src/main/java/com/example/TeacherAppServer/domain/model/User.java
+++ src/main/java/com/example/TeacherAppServer/domain/model/User.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
@@ -22,10 +24,13 @@ import java.util.List;
 public class User implements UserDetails {
     @Id
     @Column(unique = true, nullable = false, updatable = false)
+    @Size(min = 3, max = 20)
     private String username;
     @Column(nullable = false)
+    @Size(min = 5, max = 20)
     private String password;
     @Column(nullable = false)
+    @Size(min = 3, max = 15)
     private String nickname;
     @Column(updatable = false, nullable = false)
     private LocalDateTime dateCreated;
